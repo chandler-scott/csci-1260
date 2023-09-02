c2fa70c62dfca8ea37289e0749b3bc1bdb8aeca9
Chandler Scott <chandler.scott.dev@gmail.com>
2023-09-01 21:56:47 -0400

    class lib 1

---
+++ src/libs/LibObjects/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace LibObjects;
+
+public class Dog
+{
+    private string name = string.Empty;
+
+    public Dog(string name)
+    {
+        this.name = name;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+}
--- src/oop/Program.cs
+++ src/oop/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Dog dog = new Dog("Fido");
+
+
+Console.WriteLine($"Hello, {dog.getName()}!");
